a419f4599cb21e51ab7375a9bc620b30ad8aee56
alessiosmakaj <129155526+alessiosmakaj@users.noreply.github.com>
2023-08-25 15:25:16 +0200

    Update CentroControlloRepository.java

---
+++ src/main/java/com/Gestione_Incendi/Project/Repository/CentroControlloRepository.java
@@ -0,0 +1,9 @@
+package com.Gestione_Incendi.Project.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Gestione_Incendi.Project.model.CentroControllo;
+
+public interface CentroControlloRepository extends JpaRepository<CentroControllo, Long> {
+
+}
