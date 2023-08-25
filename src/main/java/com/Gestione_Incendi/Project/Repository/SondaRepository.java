768e3780527b0adb59c82ba3c0647727e49d6d81
alessiosmakaj <129155526+alessiosmakaj@users.noreply.github.com>
2023-08-25 15:24:53 +0200

    Update SondaRepository.java

---
+++ src/main/java/com/Gestione_Incendi/Project/Repository/SondaRepository.java
@@ -0,0 +1,9 @@
+package com.Gestione_Incendi.Project.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Gestione_Incendi.Project.model.Sonda;
+
+public interface SondaRepository extends JpaRepository<Sonda, Long> {
+
+}
